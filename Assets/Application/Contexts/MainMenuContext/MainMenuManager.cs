0f7c35cbde01e6247f300938ef8858951aeadcd8
Konrad Dudek <kdudek111@wp.pl>
2023-11-17 13:02:08 +0100

    Added loading screen

--- Assets/Application/Contexts/MainMenuContext/MainMenuManager.cs
+++ Assets/Application/Contexts/MainMenuContext/MainMenuManager.cs
@@ -1,15 +1,34 @@
-using System.Collections.Generic;
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Application.Contexts.MainMenuContext
 {
     public class MainMenuManager : MonoBehaviour
     {
-        [SerializeField] private List<UIPanel> _panels;
-        public void OpenGameplay()
+        [SerializeField] private GameObject _loadingScreen;
+        [SerializeField] private Image _loadingFill;
+        [SerializeField] private float _duration = 0.4f;
+
+        public void LoadScene(string sceneName)
         {
-            SceneManager.LoadScene("Gameplay");
+            _loadingScreen.SetActive(true);
+            _loadingScreen.transform.DOLocalMoveY(0f, _duration).OnComplete(() => 
+                StartCoroutine(LoadSceneAsync(sceneName)));
+        }
+
+        private IEnumerator LoadSceneAsync(string sceneName)
+        {
+            var operation = SceneManager.LoadSceneAsync(sceneName);
+
+            while (!operation.isDone)
+            {
+                var progress = Mathf.Clamp01(operation.progress / 0.9f);
+                _loadingFill.fillAmount = progress;
+                yield return null;
+            }
         }
     }
 }
