41e5f3a95849f56e657912ef9935d8d6c007edd5
Konrad Dudek <kdudek111@wp.pl>
2023-11-28 23:10:47 +0100

    Added exp idle animaiton, rock sprites and shadow util

--- Assets/ExpFollowMediator.cs
+++ Assets/ExpFollowMediator.cs
@@ -1,6 +1,8 @@
+using System;
 using Application.Contexts.GameplayContext;
 using Application.Contexts.GameplayContext.Models;
 using Application.Contexts.ProjectContext.Configs;
+using DG.Tweening;
 using UnityEngine;
 using Zenject;
 
@@ -8,8 +10,19 @@ public class ExpFollowMediator : MonoBehaviour
 {
     [Inject] private readonly CorvetGameConfig _gameConfig;
     [Inject] private readonly PlayerModel _playerModel;
+
+    [SerializeField] private GameObject _expBody;
+    [SerializeField] private GameObject _shadow;
+    [SerializeField] private float _duration;
     
     private bool _isCollected;
+
+    private void Start()
+    {
+        Debug.Log($"Will move");
+        _expBody.transform.DOLocalMoveY(0.05f, _duration).SetLoops(-1, LoopType.Yoyo);
+        _shadow.transform.DOScale(new Vector3(0.03f, 0.015f, 0),_duration).SetLoops(-1, LoopType.Yoyo);
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
